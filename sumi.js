8eb91d970ea7eea3f6b34578bf46041335944196
Austin Stotts <astotts@cdicon.com>
2023-11-21 08:45:45 -0600

    haiku 3

--- sumi.js
+++ sumi.js
@@ -8,10 +8,6 @@ let client = new Client({
     ]
 });
 
-let rng = (low=0, high=100) => {
-
-}
-
 let faces = ["(⁀ᗢ⁀)","\\(^ヮ^)/","(„• ᴗ •„)","	⸜(⸝⸝⸝´꒳`⸝⸝⸝)⸝","( = ⩊ = )","(♡˙︶˙♡)","♡＼(￣▽￣)／♡","(´꒳`)♡","	\(〇_ｏ)/","╮(︶▽︶)╭","(*°ｰ°)ﾉ","(⊃｡•́‿•̀｡)⊃","(っ ᵔ◡ᵔ)っ","(｡•̀ᴗ-)✧","	|ʘ‿ʘ)╯","☆ﾐ(o*･ω･)ﾉ","	(=^･ｪ･^=)","U・ᴥ・U","	૮₍ ˶• ༝ •˶ ₎ა","	(; ・_・)――――C","( ˘▽˘)っ♨","	-●●●-ｃ(・・ )","( ・・)つ-●●●","( o˘◡˘o) ┌iii┐","	(〜￣▽￣)〜","(~‾▽‾)~","✺◟( • ω • )◞✺","	( ͠° ͟ʖ ͡°)","( . •́ _ʖ •̀ .)","(⌐■_■)","ଘ(੭ˊᵕˋ)੭* ੈ✩‧₊˚","(ノ°∀°)ノ⌒･*:.｡. .｡.:*･゜ﾟ･*☆","	(/￣ー￣)/~~☆’.･.･:★’.･.･:☆"]
 let greeting = ["haii", "hi", "おはよう!", "おやすみ...", "こんにちは", "hey", "hello!", "greetings!", "Hola", "hi", "haaaaay", "hewwo", "HEY!", "hiiii", "boo!"];
 let leaving = ["bye", "see you!", "see you", "bye bye", "goodnight!", "goodnight", "gn", "gn!", "sweet dreams"];
@@ -20,13 +16,14 @@ client.on("ready", () => {
     console.log("sumi ready!");
 })
 
+
 client.on("messageCreate", async (message) => {
-    console.log(message)
+    // console.log(message); // uncomment to print all messages
     if (message.content.startsWith("https://x.com") || message.content.startsWith("https://twitter.com")) {
         let data = message.content.split(".com")[1];
         message.channel.send(`https://vxtwitter.com${data}`);
     }
-    else if(message.content.toLowerCase().startsWith("hey sumi") || message.content.toLowerCase().startsWith("hello sumi") || message.content.toLowerCase().startsWith("hi sumi") || message.content.toLowerCase().startsWith("wsg sumi")) {
+    else if(message.content.toLowerCase().startsWith("hey sumi") || message.content.toLowerCase().startsWith("hello sumi") || message.content.toLowerCase().startsWith("hi sumi") || message.content.toLowerCase().startsWith("wsg sumi") || message.content.toLowerCase().startsWith("gm sumi")) {
         message.react("💖");
         message.reply(`${greeting[Math.floor(Math.random()*greeting.length)]} ${faces[Math.floor(Math.random()*faces.length)]}`);
     }
@@ -34,20 +31,9 @@ client.on("messageCreate", async (message) => {
         message.react("💖");
         message.reply(`${leaving[Math.floor(Math.random()*leaving.length)]} ${faces[Math.floor(Math.random()*faces.length)]}`);
     } 
-    else if(message.author == "HaikuBot") {
+    else if(message.author.username == "HaikuBot" && message.author.bot && message.embeds.length > 0) {
         message.channel.send("great haiku");
     }
-    // else {
-    //     let command = message.content.split(" ");
-    //     if(command[0].toLowerCase() == "sumi") {
-    //         if(command[1].toLowerCase() == "rng") {
-    //             let low = command[2];
-    //             let high = command[3];
-
-                
-    //         }
-    //     }
-    // }
 })
 
 client.login(ts);
